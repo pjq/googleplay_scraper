927e2dcb0388115d3517cf9e8d1b1349fbc35656
Jianqing Peng <pengjianqing@gmail.com>
2023-02-28 16:58:59 +0800

    add lang and country support

--- googleplay_scraper.py
+++ googleplay_scraper.py
@@ -17,19 +17,19 @@ class GooglePlay:
         # print(result)
         self.write(f"app_detail_{output_file}", result)
 
-    def app_reviews_all(self):
+    def app_reviews_all(self, lang="en", country="us"):
         result = reviews_all(
             app_pkg,
             sleep_milliseconds=0,  # defaults to 0
-            lang='en',  # defaults to 'en'
-            country='us',  # defaults to 'us'
+            lang=lang,  # defaults to 'en'
+            country=country,  # defaults to 'us'
             sort=Sort.MOST_RELEVANT,  # defaults to Sort.MOST_RELEVANT
             count=30000,  # defaults to 100
             filter_score_with=None  # defaults to None(means all score)
         )
 
         # print(result)
-        self.writeArray(f"app_reviews_all_{output_file}", result)
+        self.writeArray(f"app_reviews_all_{lang}_{country}_{output_file}", result)
 
     def write(self, file="default_output.txt", result=Any):
         print(f"write {output_file}")
@@ -51,21 +51,21 @@ class GooglePlay:
                 score = review.get("score", "")
                 thumbs_up_count = review.get("thumbsUpCount", "")
                 review_text = f"userName: {user_name}, score: {score}, thumbsUpCount: {thumbs_up_count}, content: {content}"
-                output_file.write(review_text+'\n')
+                output_file.write(review_text + '\n')
                 # print(review_text)
 
-    def app_reviews(self):
+    def app_reviews(self, lang="en", country="us"):
         result, continuation_token = reviews(
             app_pkg,
-            lang='en',  # defaults to 'en'
-            country='us',  # defaults to 'us'
+            lang=lang,  # defaults to 'en'
+            country=country,  # defaults to 'us'
             sort=Sort.NEWEST,  # defaults to Sort.NEWEST
             count=3,  # defaults to 100
             filter_score_with=5  # defaults to None(means all score)
         )
 
         # print(result)
-        self.writeArray(f"app_reviews_{output_file}", result)
+        self.writeArray(f"app_reviews_{lang}_{country}_{output_file}", result)
 
 
 if __name__ == '__main__':
@@ -87,5 +87,7 @@ if __name__ == '__main__':
 
     googleplay = GooglePlay()
     googleplay.app_detail()
-    googleplay.app_reviews()
-    googleplay.app_reviews_all()
+    googleplay.app_reviews(lang="en", country="us")
+    googleplay.app_reviews(lang="zh", country="CN")
+    googleplay.app_reviews_all(lang="en", country="us")
+    googleplay.app_reviews_all(lang="zh", country="CN")
